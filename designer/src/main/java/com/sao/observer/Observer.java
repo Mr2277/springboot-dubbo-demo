7adbdb33a9556af5586675f89df014a063eb0edb
sun <837544713@qq.com>
2021-06-04 23:54:01 +0800

    观察者模式

---
+++ designer/src/main/java/com/sao/observer/Observer.java
@@ -0,0 +1,7 @@
+package com.sao.observer;
+
+public interface Observer {
+
+    void update();
+
+}
---
+++ designer/src/main/java/com/sao/observer/Subject.java
@@ -0,0 +1,24 @@
+package com.sao.observer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class Subject {
+
+    private List<Observer> observerList = new ArrayList<>();
+
+    public void addObserver(Observer observer) {
+        this.observerList.add(observer);
+    }
+
+    public void removeObserver(Observer observer) {
+        this.observerList.remove(observer);
+    }
+
+    public void notifyObserver() {
+        for (Observer observer : observerList) {
+            observer.update();
+        }
+    }
+
+}
